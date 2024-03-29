6ddca2dd1b8fd0c642df5a4485d99f286134d254
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2024-03-29 13:10:05 -0400

    added authorization validation

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,28 +1,34 @@
 const DailyHealthInfo = require('../models/dailyHealthInfoModel');
 const PatientData = require('../models/patientDataModel');
+const {NURSE, PATIENT} = require("../enums/roleEnum");
 
 const resolvers = {
   Query: {
     getDailyHealthInfo: async (_, { id }) => {
       return await DailyHealthInfo.findById(id);
     },
-    getDailyHealthInfos: async () => {
+    /*getDailyHealthInfos: async () => {
       return await DailyHealthInfo.find();
-    },
+    },*/
     getDailyHealthInfosByUser: async (_, { userId }) => {
       return await DailyHealthInfo.find({ user: userId });
     },
-    getPatientData: async (_, { userId }) => {
+/*    getPatientData: async (_, { userId }) => {
       return await PatientData.findOne({ user: userId });
-    },
-    getPatientDataByUser: async (_, { userId }) => {
+    },*/
+    getPatientDataByUser: async (_, { userId }, context) => {
+      if (!context.user) throw new Error(JSON.stringify({message: 'You are not authenticated!', code: 401}));
+      else if (context.user.profile !==NURSE) throw new Error(JSON.stringify({
+        message: 'You are not authorized to add this data!',
+        code: 403
+      }));
       return await PatientData.find({ user: userId });
     },
   },
   Mutation: {
     addDailyHealthInfo: async (_, { user, pulseRate, bloodPressure, weight, bodyTemperature, respiratoryRate }, context) => {
         if (!context.user) throw new Error(JSON.stringify({message: 'You are not authenticated!', code: 401}));
-        else if (context.user.profile !== 'nurse') throw new Error(JSON.stringify({
+        else if (context.user.profile !== PATIENT) throw new Error(JSON.stringify({
             message: 'You are not authorized to add this data!',
             code: 403
         }));
@@ -39,7 +45,7 @@ const resolvers = {
     },
     addPatientData: async (_, { user, vitalSigns, symptoms }, context) => {
       if (!context.user) throw new Error(JSON.stringify({message: 'You are not authenticated!', code: 401}));
-      else if (context.user.profile !== 'nurse') throw new Error(JSON.stringify({
+      else if (context.user.profile !==NURSE) throw new Error(JSON.stringify({
         message: 'You are not authorized to add this data!',
         code: 403
       }));
