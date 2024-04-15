35fd6fadb8c6d44252c53fb9e179602e9f36a1dd
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2024-04-14 22:43:10 -0400

    changed to render backend

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
-//export const host = "https://wecanchat-nc8w.onrender.com";
-export const host = "http://localhost:5000";
+export const host = "https://comp308-group-project.onrender.com";
+//export const host = "http://localhost:5000";
 export const grapqhQl = `${host}/api/graphql`;
 export const loginRoute = `${host}/api/users/login`;
 export const registerRoute = `${host}/api/users`;
